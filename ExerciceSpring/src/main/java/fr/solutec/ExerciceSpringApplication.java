688ac05158ac2dfe9a531d3c3525d5c3962ad7ef
arthur <arthur@LAPTOP-SIPFHS9J>
2021-10-26 16:49:20 +0200

    Création des données dans la table user

--- ExerciceSpring/src/main/java/fr/solutec/ExerciceSpringApplication.java
+++ ExerciceSpring/src/main/java/fr/solutec/ExerciceSpringApplication.java
@@ -1,14 +1,36 @@
 package fr.solutec;
 
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.solutec.entities.User;
+import fr.solutec.repository.UserRepository;
+
 @SpringBootApplication
-public class ExerciceSpringApplication {
+public class ExerciceSpringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepos;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExerciceSpringApplication.class, args);
 		System.out.println("lancement terminé");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		User u1 = new User(null, "CORNET", "Arthur", "arthurc", "1234");
+		User u2 = new User(null, "MSAIDIE", "Zaher", "exploit", "abcd");
+		User u3 = new User (null, "EUSTACHE", "Juliette", "juliettee", "rose");
+		User u4 = new User (null, "GALLON", "Rose-Marie", "rosemarieg", "arbre");
+		Stream.of(u1, u2, u3, u4).forEach(u -> {
+			userRepos.save(u);
+		});
 	}
 
 }
--- ExerciceSpring/src/main/java/fr/solutec/entities/Memo.java
+++ ExerciceSpring/src/main/java/fr/solutec/entities/Memo.java
@@ -3,19 +3,22 @@ package fr.solutec.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-
 @NoArgsConstructor @AllArgsConstructor @Data
 public class Memo {
 	@Id @GeneratedValue
 	private Long id;
 	private String msg;
 	private String statut;
+	@OneToOne
 	private User destinataire;
-
+	@ManyToOne
+	private User user;
 }
