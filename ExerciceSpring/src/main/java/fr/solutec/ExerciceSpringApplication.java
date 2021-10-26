4e1c01145c94eb756307137c5407f156fd02afd9
rosem <rosem@LAPTOP-JRBMPSI9>
2021-10-26 15:21:13 +0200

    base du programme

---
+++ ExerciceSpring/src/main/java/fr/solutec/ExerciceSpringApplication.java
@@ -0,0 +1,14 @@
+package fr.solutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciceSpringApplication.class, args);
+		System.out.println("lancement terminé");
+	}
+
+}
