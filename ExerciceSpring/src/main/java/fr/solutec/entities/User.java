27a45b7bb300f271209166ace1a7fbbfd4deca06
arthur <arthur@LAPTOP-SIPFHS9J>
2021-10-26 15:39:53 +0200

    Creation classe user

--- ExerciceSpring/src/main/java/fr/solutec/entities/User.java
+++ ExerciceSpring/src/main/java/fr/solutec/entities/User.java
@@ -3,6 +3,7 @@ package fr.solutec.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,4 +18,7 @@ public class User {
 	private String prenom;
 	private String login;
 	private String mdp;
+	
+	@OneToMany
+	private Memo memo;
 }
